7fe858d95eaa8329a8247cb5188a6353a64519d6
meli <melatbeyene04@gmail.com>
2017-07-02 22:56:54 -0700

    Update Assignment2KiloMile.py

--- Assignment2KiloMile.py
+++ Assignment2KiloMile.py
@@ -1,6 +1,5 @@
 #Assignment 2
-def main():
-  
+def main():  
   #converting km to mile
     print(" convert kilometers to miles")
     num = eval (input (" please enter number in kilometer "))
@@ -8,13 +7,12 @@ def main():
         num = num * 0.62137
         print(num)
         
-    #converting mile to km    
+   #converting mile to km    
     print(" convert miles to kilometers")
     num = eval (input (" please enter number in miles "))
     for j in range(1):
         num = num * 1.609344
         print(num)
-
 
 main()
 
